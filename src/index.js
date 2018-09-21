f7cf6d82a1d3d96ac635495b7b6165ffa52a1eea
Nicolae Matei <matei.nick@gmail.com>
2018-09-21 19:39:26 +0300

    Implementing Store from Scratch

--- src/index.js
+++ src/index.js
@@ -10,7 +10,29 @@ const counter = (state = 0, action) => {
   }
 };
 
-const { createStore } = Redux;
+//const { createStore } = Redux;
+const createStore = reducer => {
+  let state;
+  let listeners = [];
+
+  const getState = () => state;
+
+  const dispatch = action => {
+    state = reducer(state, action);
+    listeners.forEach(listener => listener());
+  };
+
+  const subscribe = listener => {
+    listeners.push(listener);
+    return () => {
+      listeners = listeners.firlter(f => f !== listener);
+    };
+  };
+
+  dispatch({});
+  return { getState, dispatch, subscribe };
+};
+
 const store = createStore(counter);
 
 const render = () => {
